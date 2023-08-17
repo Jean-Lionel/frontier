31addef64f1d82b640c5e0bfe6d9c88ddba655ba
Jean-Lionel <nijeanlionel@gmail.com>
2023-08-17 06:03:27 +0200

    first commit

---
+++ src/Frontier.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) VOTRE NOM <nijeanlionel@email.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+// If you don't to add a custom vendor folder, then use the simple class
+// namespace Frontier;
+namespace Frontier;
+
+class Frontier
+{
+    public function hey($salut = "Rien")
+    {
+        return $salut;
+    }
+}
